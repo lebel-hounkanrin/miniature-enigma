5c25d52625b83e9dd910bf1ec6d214e99217e93e
neil <lebelhounkanrin@gmail.com>
2024-12-15 13:15:06 +0100

    feat: add authorization

---
+++ Helpers/AuthorizeAttribute.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using parc.Models;
+
+namespace parc.Helpers;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class AuthorizeAttribute: Attribute, IAuthorizationFilter
+{
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        var user = (CustomUser?)context.HttpContext.Items["User"];
+        if (user == null)
+        {
+            context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
+        }
+    }
+}
